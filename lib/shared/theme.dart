e179700e7881aa28a298ac2c3ddd6aad6b2b8361
Renaldhi Fahrezi <renaldhifahrezi48@gmail.com>
2022-12-03 16:58:37 +0800

    adding theme, landing page UI done

---
+++ lib/shared/theme.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// Colors
+const Color primaryColor = Color(0xFF075A30);
+const Color secondaryColor = Color(0xFFF17532);
+const Color greyColor = Color(0xFF555555);
+const Color kUnavailableColor = Color(0xFFFAFAFA);
+const Color whiteColor = Color(0xFFFFFFFF);
+
+// Text Styles
+TextStyle mainTextStyle = GoogleFonts.inter(
+  color: greyColor,
+);
+
+TextStyle greenTextStyle = GoogleFonts.inter(
+  color: primaryColor,
+);
+
+TextStyle orangeTextStyle = GoogleFonts.inter(
+  color: secondaryColor,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.inter(
+  color: whiteColor,
+);
+
+// Font Weights
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
+
+// Margins
+double defaultMargin = 36;
+double defaultRadius = 12;
---
+++ lib/ui/pages/landing_page.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:loh_coffee_eatery/ui/widgets/custom_button.dart';
+import '/shared/theme.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: whiteColor,
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                // Orange list
+                Container(
+                  width: double.infinity,
+                  height: 600,
+                  decoration: BoxDecoration(
+                    color: secondaryColor,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(defaultRadius),
+                      bottomRight: Radius.circular(defaultRadius),
+                    ),
+                  ),
+                ),
+    
+                // Cover Image
+                SizedBox(
+                  height: 595,
+                  width: double.infinity,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(defaultRadius),
+                      bottomRight: Radius.circular(defaultRadius),
+                    ),
+                    child: Image.asset(
+                      'assets/images/landing_page.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 52),
+              child: Column(
+                children: [
+                  CustomButton(
+                    title: 'Sign Up', 
+                    onPressed: (){}
+                    ),
+                  const SizedBox(height: 5),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Already have an account? ',
+                          style: orangeTextStyle.copyWith(
+                            fontSize: 16,
+                            fontWeight: regular,
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: (){},
+                          child: Text(
+                            'Sign In', style: greenTextStyle.copyWith(
+                            fontSize: 16,
+                            fontWeight: bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
