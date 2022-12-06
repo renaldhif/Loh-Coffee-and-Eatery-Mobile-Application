a290e72603d9002c26bad5f35c995705dc932b1d
Renaldhi Fahrezi <renaldhifahrezi48@gmail.com>
2022-12-06 13:30:02 +0800

    adding new pages

---
+++ lib/ui/pages/signup_page.dart
@@ -0,0 +1,329 @@
+import 'package:flutter/material.dart';
+import '../widgets/custom_button.dart';
+import '/shared/theme.dart';
+import 'package:intl/intl.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  // TextEditingControllers
+  final TextEditingController _fullnameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _dateInputController = TextEditingController();
+  // Boolean to show hide password
+  bool _isObsecure = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: whiteColor,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                // Orange list
+                Container(
+                  width: double.infinity,
+                  height: 400,
+                  decoration: BoxDecoration(
+                    color: secondaryColor,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(defaultRadius),
+                      bottomRight: Radius.circular(defaultRadius),
+                    ),
+                  ),
+                ),
+
+                // Cover Image
+                SizedBox(
+                  height: 395,
+                  width: double.infinity,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(defaultRadius),
+                      bottomRight: Radius.circular(defaultRadius),
+                    ),
+                    child: Image.asset(
+                      'assets/images/signup.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Email
+                  Text(
+                    'Email address',
+                    style: mainTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: bold,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    height: 50,
+                    child: TextFormField(
+                      controller: _emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        floatingLabelBehavior: FloatingLabelBehavior.never,
+                        hintText: 'Enter your email',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(defaultRadius),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(defaultRadius),
+                          borderSide: const BorderSide(
+                            color: primaryColor,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(defaultRadius),
+                          borderSide: const BorderSide(
+                            color: primaryColor,
+                            width: 1.5,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(
+                    height: 15,
+                  ),
+
+                  // Password
+                  Text(
+                    'Password',
+                    style: mainTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: bold,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    height: 50,
+                    child: TextFormField(
+                      controller: _passwordController,
+                      obscureText: _isObsecure,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        hintText: 'Enter your password',
+                        floatingLabelBehavior: FloatingLabelBehavior.never,
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _isObsecure = !_isObsecure;
+                            });
+                          },
+                          icon: Icon(
+                            _isObsecure
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                            color: _isObsecure ? greyColor : primaryColor,
+                          ),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(defaultRadius),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(defaultRadius),
+                          borderSide: const BorderSide(
+                            color: primaryColor,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(defaultRadius),
+                          borderSide: const BorderSide(
+                            color: primaryColor,
+                            width: 1.5,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  // Forgot Password
+                  // Container(
+                  //   alignment: Alignment.centerRight,
+                  //   child: SizedBox(
+                  //     height: 30,
+                  //     child: TextButton(
+                  //       onPressed: (){},
+                  //       child: Text('Forgot Password?',
+                  //       style: greenTextStyle.copyWith(
+                  //         fontWeight: medium,
+                  //         ),
+                  //       ),
+                  //     ),
+                  //   ),
+                  // ),
+
+                  const SizedBox(
+                    height: 15,
+                  ), // Email
+                  Text(
+                    'Full Name',
+                    style: mainTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: bold,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    height: 50,
+                    child: TextFormField(
+                      controller: _fullnameController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: 'Full Name',
+                        floatingLabelBehavior: FloatingLabelBehavior.never,
+                        hintText: 'Enter your full name',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(defaultRadius),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: const BorderSide(
+                            color: primaryColor,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: const BorderSide(
+                            color: primaryColor,
+                            width: 1.5,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  // Email
+                  Text(
+                    'Date of Birth',
+                    style: mainTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: bold,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    height: 50,
+                    child: TextFormField(
+                      controller: _dateInputController,
+                      keyboardType: TextInputType.datetime,
+                      readOnly: true,
+                      onTap: () async {
+                        DateTime? pickedDate = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(1950),
+                          lastDate: DateTime(2050),
+                          builder: (context, child) {
+                            return Theme(
+                              data: ThemeData.light().copyWith(
+                                colorScheme: const ColorScheme.light(
+                                  primary: primaryColor,
+                                ),
+                                buttonTheme: const ButtonThemeData(
+                                  textTheme: ButtonTextTheme.primary,
+                                ),
+                              ),
+                              child: child!,
+                            );
+                          },
+                        );
+                        if (pickedDate != null) {
+                          var formatDate = DateTime.parse(pickedDate.toString());
+                          var formattedDate = "${formatDate.day}-${formatDate.month}-${formatDate.year}";
+                          _dateInputController.text = formattedDate;
+                        }
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Date of Birth',
+                        floatingLabelBehavior: FloatingLabelBehavior.never,
+                        hintText: 'Enter your date of birth',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: const BorderSide(
+                            color: primaryColor,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: const BorderSide(
+                            color: primaryColor,
+                            width: 1.5,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(height: 20),
+                  CustomButton(
+                    title: 'Sign Up',
+                    onPressed: () {
+                      //* TODO: Create Account
+                      // Navigator.pushNamed(context, '/');
+                    },
+                  ),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Already have an account?',
+                        style: orangeTextStyle.copyWith(
+                          fontSize: 16,
+                          fontWeight: regular,
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/login');
+                        },
+                        child: Text(
+                          'Sign In',
+                          style: greenTextStyle.copyWith(
+                            fontSize: 16,
+                            fontWeight: bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
