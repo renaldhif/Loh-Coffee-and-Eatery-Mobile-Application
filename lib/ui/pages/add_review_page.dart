63c6d82c71c5be7bbce6f3c76d9aaa7cc3852f32
Renaldhi Fahrezi <renaldhifahrezi48@gmail.com>
2022-12-11 19:37:16 +0800

    review pages

--- lib/ui/pages/add_review_page.dart
+++ lib/ui/pages/add_review_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:loh_coffee_eatery/shared/theme.dart';
+import 'package:loh_coffee_eatery/ui/widgets/custom_button.dart';
+
+import '../widgets/custom_textformfield.dart';
 
 class AddReviewPage extends StatefulWidget {
   const AddReviewPage({super.key});
@@ -10,10 +15,148 @@ class AddReviewPage extends StatefulWidget {
 }
 
 class _AddReviewPageState extends State<AddReviewPage> {
+  // TextEditingControllers
+  final TextEditingController _reviewController = TextEditingController();
+
+  Widget starRating() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 30),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RatingBar.builder(
+                initialRating: 3,
+                minRating: 1,
+                direction: Axis.horizontal,
+                allowHalfRating: true,
+                itemCount: 5,
+                itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+                itemBuilder: (context, _) => const Icon(
+                  Icons.star,
+                  color: primaryColor,
+                ),
+                onRatingUpdate: (rating) {
+                  print(rating);
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget reviewTextFormField() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 15),
+          SizedBox(
+            width: double.infinity,
+            // height: 100,
+            child: TextFormField(
+              controller: _reviewController,
+              minLines: 5,
+              maxLines: 5,
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                  vertical: 15,
+                ),
+                labelText: 'Input Review', // this field is required
+                floatingLabelBehavior: FloatingLabelBehavior.never,
+                hintText: 'Submit your review here', // this field is required
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: const BorderSide(
+                    color: primaryColor,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: const BorderSide(
+                    color: primaryColor,
+                    width: 1.5,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Container(
+        color: whiteColor,
+        child: Column(
+          children: [
+            // Header
+            Container(
+              width: double.infinity,
+              margin: const EdgeInsets.only(top: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: const Icon(
+                      Icons.arrow_circle_left_rounded,
+                      color: primaryColor,
+                      size: 55,
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 12),
+                    child: Text(
+                      'Submit Review',
+                      style: greenTextStyle.copyWith(
+                        fontSize: 40,
+                        fontWeight: bold,
+                      ),
+                    ),
+                  ),
+
+                  // Rating Content
+                  //* Star Rating
+                  starRating(),
+                  const SizedBox(height: 15),
+
+                  // Submit Review Content
+                  //* Review
+                  reviewTextFormField(),
+
+                  // Submit Button
+
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 50),
+                    child: CustomButton(
+                      title: 'Submit Review',
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/ui/pages/review_page_admin.dart
+++ lib/ui/pages/review_page_admin.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:loh_coffee_eatery/ui/widgets/custom_rating_card.dart';
+import '../../shared/theme.dart';
+
 
 class ReviewPageAdmin extends StatefulWidget {
   const ReviewPageAdmin({super.key});
@@ -13,7 +14,81 @@ class _ReviewPageAdminState extends State<ReviewPageAdmin> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: SingleChildScrollView(
+        child: Container(
+          color: whiteColor,
+          width: double.infinity,
+          child: Column(
+            children: [
+              // Header
+              Container(
+                width: double.infinity,
+                margin: const EdgeInsets.only(top: 30),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(
+                        Icons.arrow_circle_left_rounded,
+                        color: primaryColor,
+                        size: 55,
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 12),
+                      child: Text(
+                        'Reviews',
+                        style: greenTextStyle.copyWith(
+                          fontSize: 40,
+                          fontWeight: bold,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                  ],
+                ),
+              ), // Header End
+              
+              //* Content
+              CustomRatingCard(
+                username: 'John Doe',
+                email: 'johndoe@gmail.com',
+                review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt luctus, nunc nisl aliquam nunc, vel aliquam nunc nisl euismod nunc. Sed euismod, nunc vel tincidunt luctus, nunc nisl aliquam nunc, vel aliquam nunc nisl euismod nunc.',
+                rating: 5,
+                date: DateTime.now(),
+              ),
+
+              CustomRatingCard(
+                username: 'John Doe',
+                email: 'johndoe@gmail.com',
+                review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt luctus, nunc nisl aliquam nunc, vel aliquam nunc nisl euismod nunc. Sed euismod, nunc vel tincidunt luctus, nunc nisl aliquam nunc, vel aliquam nunc nisl euismod nunc.',
+                rating: 5,
+                date: DateTime.now(),
+              ),
+
+              CustomRatingCard(
+                username: 'John Doe',
+                email: 'johndoe@gmail.com',
+                review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt luctus, nunc nisl aliquam nunc, vel aliquam nunc nisl euismod nunc. Sed euismod, nunc vel tincidunt luctus, nunc nisl aliquam nunc, vel aliquam nunc nisl euismod nunc.',
+                rating: 5,
+                date: DateTime.now(),
+              ),
+
+              CustomRatingCard(
+                username: 'John Doe',
+                email: 'johndoe@gmail.com',
+                review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt luctus, nunc nisl aliquam nunc, vel aliquam nunc nisl euismod nunc. Sed euismod, nunc vel tincidunt luctus, nunc nisl aliquam nunc, vel aliquam nunc nisl euismod nunc.',
+                rating: 5,
+                date: DateTime.now(),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
