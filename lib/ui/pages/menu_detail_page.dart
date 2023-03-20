96e4d15437cbfd858bd00b5aee5183fc4802092d
Renaldhi Fahrezi <renaldhifahrezi48@gmail.com>
2023-03-21 00:58:09 +0800

    adding menu details

---
+++ lib/ui/pages/menu_detail_page.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:loh_coffee_eatery/cubit/menu_cubit.dart';
+import 'package:loh_coffee_eatery/models/menu_model.dart';
+import '../../cubit/table_cubit.dart';
+import '/shared/theme.dart';
+
+class MenuDetailPage extends StatefulWidget {
+  MenuModel? inMenu;
+  MenuDetailPage({super.key, this.inMenu});
+
+  @override
+  State<MenuDetailPage> createState() => _MenuDetailPageState();
+}
+
+class _MenuDetailPageState extends State<MenuDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          color: whiteColor,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Header
+              Container(
+                width: double.infinity,
+                margin: const EdgeInsets.only(top: 30),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(
+                        Icons.arrow_circle_left_rounded,
+                        color: primaryColor,
+                        size: 55,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              // The detail of the menu selected
+              Text(
+                'Menu Detail',
+                style: greenTextStyle.copyWith(
+                  fontSize: 22,
+                  fontWeight: black,
+                ),
+              ),
+              const SizedBox(height: 15),
+              // image
+              Container(
+                width: 0.9 * MediaQuery.of(context).size.width,
+                height: 400,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(widget.inMenu!.image),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15),
+              // name
+              Text(
+                widget.inMenu!.title,
+                style: greenTextStyle.copyWith(
+                  fontSize: 22,
+                  fontWeight: black,
+                ),
+              ),
+              const SizedBox(height: 15),
+              // price
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Rp ',
+                    style: orangeTextStyle.copyWith(
+                      fontSize: 22,
+                      fontWeight: black,
+                    ),
+                  ),
+                  Text(
+                    widget.inMenu!.price.toString(),
+                    style: orangeTextStyle.copyWith(
+                      fontSize: 22,
+                      fontWeight: black,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 15),
+              // description
+              Text(
+                widget.inMenu!.description,
+                style: greenTextStyle.copyWith(
+                  fontSize: 16,
+                  fontWeight: medium,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 30),
+              // tag
+              Text(
+                'Tag: ${widget.inMenu!.tag}',
+                style: greenTextStyle.copyWith(
+                  fontSize: 16,
+                  fontWeight: medium,
+                ),
+              ),
+              const SizedBox(height: 15),
+              // total loved
+              Text(
+                'Total Loved: ${widget.inMenu!.totalLoved}',
+                style: greenTextStyle.copyWith(
+                  fontSize: 16,
+                  fontWeight: medium,
+                ),
+              ),
+              const SizedBox(height: 15),
+              // total ordered
+              Text(
+                'Total Ordered: ${widget.inMenu!.totalOrdered}',
+                style: greenTextStyle.copyWith(
+                  fontSize: 16,
+                  fontWeight: medium,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/widgets/custom_card_menu_item.dart
+++ lib/ui/widgets/custom_card_menu_item.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:loh_coffee_eatery/ui/pages/home_page.dart';
+import 'package:loh_coffee_eatery/ui/pages/menu_detail_page.dart';
 import '../../cubit/menu_cubit.dart';
 import '../../models/menu_model.dart';
 import '/shared/theme.dart';
@@ -11,17 +12,9 @@ List _cartItems = [];
 class CustomCardMenuItem extends StatefulWidget {
   final MenuModel menu;
 
-
   CustomCardMenuItem(
     this.menu, {
     super.key,
-    // required this.title,
-    // required this.image,
-    // required this.tag,
-    // required this.price,
-    // required this.qtyLoved,
-    // required this.qtyOrdered,
-    // required this.onPressed,
   });
 
   @override
@@ -29,7 +22,6 @@ class CustomCardMenuItem extends StatefulWidget {
 }
 
 class _CustomCardMenuItemState extends State<CustomCardMenuItem> {
-  // final String title, image, tag, price;
   void _addToCart() {
     _cartItems.add(widget.menu);
   }
@@ -40,189 +32,222 @@ class _CustomCardMenuItemState extends State<CustomCardMenuItem> {
   @override
   Widget build(BuildContext context) {
     // Menu Card
-    return Container(
-      margin: const EdgeInsets.symmetric(
-        vertical: 3,
-      ),
-      width: 0.9 * MediaQuery.of(context).size.width,
-      height: 140,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(defaultRadius),
-        border: Border.all(
-          color: kUnavailableColor,
-          width: 1,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => MenuDetailPage(
+          inMenu: widget.menu,
+        )));
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(
+          vertical: 3,
         ),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.3),
-            spreadRadius: 3,
-            blurRadius: 7,
-            offset: const Offset(1, 3),
+        width: 0.9 * MediaQuery.of(context).size.width,
+        height: 150,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(defaultRadius),
+          border: Border.all(
+            color: kUnavailableColor,
+            width: 1,
           ),
-        ],
-        color: whiteColor,
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-
-        // Menu Card Content
-        child: Row(
-          children: [
-            // Image
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(defaultRadius),
-                border: Border.all(
-                  color: Colors.grey.withOpacity(0.3),
-                  width: 1,
-                ),
-              ),
-              child: AspectRatio(
-                aspectRatio: 1 / 1,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(defaultRadius),
-                  child: Image.network(
-                    widget.menu.image, // this field is required
-                    width: 0.3 * MediaQuery.of(context).size.width,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(width: 10),
-            // Menu Details
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        widget.menu.title, // this field is required
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                        softWrap: false,
-                        style: greenTextStyle.copyWith(
-                          fontSize: 14,
-                          fontWeight: black,
-                        ),
-                      ),
-                      const SizedBox(height: 5),
-                      Text(
-                        widget.menu.tag, // this field is required
-                        style: greenTextStyle.copyWith(
-                          fontSize: 12,
-                          fontWeight: light,
-                        ),
-                      ),
-                      const SizedBox(height: 5),
-                      Row(
-                        children: [
-                          Text(
-                            'Rp',
-                            style: orangeTextStyle.copyWith(
-                              fontSize: 16,
-                              fontWeight: extraBold,
-                            ),
-                          ),
-                          const SizedBox(width: 5),
-                          Text(
-                            widget.menu.price.toString(), // this field is required
-                            style: orangeTextStyle.copyWith(
-                              fontSize: 16,
-                              fontWeight: extraBold,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  // Menu Card Button
-                  Row(
-                    children: [
-                      // Add to Cart Button
-                      GestureDetector(
-                        child: Container(
-                          width: 0.35 * MediaQuery.of(context).size.width,
-                          height: 25,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(5),
-                            border: Border.all(
-                              color: primaryColor,
-                              width: 1,
-                            ),
-                            color: whiteColor,
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              const Icon(
-                                Icons.add,
-                                color: primaryColor,
-                              ),
-                              const SizedBox(width: 10),
-                              Text(
-                                'Add to Cart',
-                                style: greenTextStyle.copyWith(
-                                  fontSize: 12,
-                                  fontWeight: extraBold,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        onTap: () {
-                          // implement ontap function add to cart
-
-                          // _cartItems.add(menu);
-                          // double totalPrice = 0;
-                          // // print all the items in the cart using loops
-                          // for (var i = 0; i < _cartItems.length; i++) {
-                          //   totalPrice += _cartItems[i].price;
-                          //   print(totalPrice);
-                          // }
-                          //add menu to the cart list
-                          _addToCart();
-                          double totalPrice = 0;
-                          for (int i = 0; i < _cartItems.length; i++) {
-                            totalPrice += _cartItems[i].price;
-                          }
-                          print(_cartItems.length);
-                          print(totalPrice);
-
-                          //_shoppingBox.put(1,menu);
-                          
-                          localDBBox.add(widget.menu);
-                          // context.read<MenuCubit>().addQuantity(
-                          //               widget.menu);
-                          
-                          //refresh page
-                          Navigator.pushAndRemoveUntil(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const HomePage(),
-                            ),
-                            (route) => false,
-                          );
-                        },
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      // Favorite Button
-                      const Icon(
-                        Icons.favorite_border_rounded,
-                        color: primaryColor,
-                        size: 28,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.3),
+              spreadRadius: 3,
+              blurRadius: 7,
+              offset: const Offset(1, 3),
             ),
           ],
+          color: whiteColor,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+
+          // Menu Card Content
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Image
+              Container(
+                width: 0.3 * MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(defaultRadius),
+                  border: Border.all(
+                    color: Colors.grey.withOpacity(0.3),
+                    width: 1,
+                  ),
+                ),
+                child: AspectRatio(
+                  aspectRatio: 1 / 1,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                    child: Image.network(
+                      widget.menu.image, // this field is required
+                      width: 0.2 * MediaQuery.of(context).size.width,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 10),
+              // Menu Details
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          widget.menu.title, // this field is required
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          softWrap: false,
+                          style: greenTextStyle.copyWith(
+                            fontSize: 14,
+                            fontWeight: black,
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                          widget.menu.tag, // this field is required
+                          style: greenTextStyle.copyWith(
+                            fontSize: 12,
+                            fontWeight: light,
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        Row(
+                          children: [
+                            Text(
+                              'Rp',
+                              style: orangeTextStyle.copyWith(
+                                fontSize: 16,
+                                fontWeight: extraBold,
+                              ),
+                            ),
+                            const SizedBox(width: 5),
+                            Text(
+                              widget.menu.price
+                                  .toString(), // this field is required
+                              style: orangeTextStyle.copyWith(
+                                fontSize: 16,
+                                fontWeight: extraBold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 5),
+                        // Loved 
+                        Row(
+                          children: [
+                            const Icon(
+                              Icons.favorite,
+                              color: primaryColor,
+                              size: 20,
+                            ),
+                            const SizedBox(width: 5),
+                            Text(
+                              widget.menu.totalLoved
+                                  .toString(), // this field is required
+                              style: greenTextStyle.copyWith(
+                                fontSize: 12,
+                                fontWeight: light,
+                              ),
+                            ),
+
+                            // Ordered
+                            const SizedBox(width: 50),
+                            const Icon(
+                              Icons.shopping_cart,
+                              color: primaryColor,
+                              size: 20,
+                            ),
+                            const SizedBox(width: 5),
+                            Text(
+                              widget.menu.totalOrdered
+                                  .toString(), // this field is required
+                              style: greenTextStyle.copyWith(
+                                fontSize: 12,
+                                fontWeight: light,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    // Menu Card Button
+                    Row(
+                      children: [
+                        // Add to Cart Button
+                        GestureDetector(
+                          child: Container(
+                            width: 0.35 * MediaQuery.of(context).size.width,
+                            height: 25,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              border: Border.all(
+                                color: primaryColor,
+                                width: 1,
+                              ),
+                              color: whiteColor,
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Icon(
+                                  Icons.add,
+                                  color: primaryColor,
+                                ),
+                                const SizedBox(width: 10),
+                                Text(
+                                  'Add to Cart',
+                                  style: greenTextStyle.copyWith(
+                                    fontSize: 12,
+                                    fontWeight: extraBold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          onTap: () {
+                            _addToCart();
+                            double totalPrice = 0;
+                            for (int i = 0; i < _cartItems.length; i++) {
+                              totalPrice += _cartItems[i].price;
+                            }
+                            print(_cartItems.length);
+                            print(totalPrice);
+
+                            localDBBox.add(widget.menu);
+
+                            //refresh page
+                            Navigator.pushAndRemoveUntil(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const HomePage(),
+                              ),
+                              (route) => false,
+                            );
+                          },
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        // Favorite Button
+                        const Icon(
+                          Icons.favorite_border_rounded,
+                          color: primaryColor,
+                          size: 28,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
